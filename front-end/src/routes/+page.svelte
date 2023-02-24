8f924a61087be2998add834d2ab9f887afa2d7f6
Sumeir Khinda <mail@sumeir.dev>
2023-02-24 13:26:49 -0800

    seperate backend and frontend

---
+++ front-end/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+
+<div class ="screen">
+    <button class="workout">Start workout<button>
+    <!-- <button></button> component -->
+</div>
+    <style>
+ button{
+    text-align: center;
+    margin-left: 33vw;
+    
+ }
+    
+    </style>
+        
